5a9147e0ebb8a600effacdc8236396b4c2a37921
Felipe <felipeluis.6@gmail.com>
2023-09-12 18:00:25 -0300

    fix: site url and img

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Felipe Luis',
     description:
       'Felipe Luis is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https:///felipeluis.com.br', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@felipeluisss',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
